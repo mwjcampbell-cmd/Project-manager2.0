7952d9f3debe1f667d88d4d9763c06d7170b2d28
mwjcampbell-cmd <mwjcampbell@gmail.com>
2025-08-31 13:05:54 +1200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const cacheName='builderPM-v1';
+const filesToCache=['index.html','project.html','style.css','app.js','manifest.json','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
